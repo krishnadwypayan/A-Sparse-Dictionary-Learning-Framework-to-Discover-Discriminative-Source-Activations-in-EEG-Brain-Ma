9db469e82005adf31cf584d19adb010a8e6f39f5
Krishna Dwypayan <32203989+krishnadwypayan@users.noreply.github.com>
2018-04-30 01:56:23 +0530

    Add files via upload

---
+++ data_extract.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def extract():
+    f = open('Competition_train_cnt.txt', 'r')
+    li = []
+    count = 0
+    X = []
+    for line in f:
+        if count == 64:
+            li.append(X)
+            X = []
+            count = 0
+
+        vals = line[:-1].strip(" ").split(" ")
+        row = []
+        for v in vals:
+            if v == " " or v == '':
+                continue
+            else:
+                row.append(float(v))
+        X.append(row)
+        count += 1
+    li.append(X)
+
+    print(np.array(li).shape)
+    return li
+
+def extract_labels():
+    f = open('Competition_train_lab.txt', 'r')
+    labels = []
+    for line in f:
+        labels.append(int(line))
+    return labels
